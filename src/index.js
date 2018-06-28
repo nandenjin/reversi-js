a3942937b06d0f00486715b1547a1c23f561daee
Kazumi Inada <timer.ground.gm@gmail.com>
2018-06-28 12:16:18 +0900

    Preparation of gh-pages branch

--- src/index.js
+++
@@ -1,6 +0,0 @@
-
-export { Cursor } from './core/Cursor.js';
-export { Board } from './core/Board.js';
-
-export { Renderer } from './renderers/Renderer.js';
-export { HTMLRenderer } from './renderers/HTMLRenderer.js';
